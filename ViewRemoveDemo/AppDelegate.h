1390e39d559f11288386d08ca31da09ef046494c
DuWenLiang <739285514@qq.com>
2017-09-11 14:39:54 +0800

    Initial Commit

---
+++ ViewRemoveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewRemoveDemo
+//
+//  Created by 杜文亮 on 2017/9/11.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewRemoveDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewRemoveDemo
+//
+//  Created by 杜文亮 on 2017/9/11.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
